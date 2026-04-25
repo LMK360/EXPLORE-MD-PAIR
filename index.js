9a96382d7e343f558eb09557b721f494287a07d2
LMK360 <lmkfail080@gmail.com>
2026-04-25 14:47:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 4000;
-let code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`Dont forget to star and fork my repo\nGitHub: @nokwandakagwe/EXPLORE-MD-PAIR\nAuthor: REDDRAGON\n\nDFS is running on running on http://localhost:` + PORT)
-})
+app.use(express.static(path.join(__dirname, 'assets')));
 
-module.exports = app
+// Routes
+const pairRoute = require('./pair');
+app.use('/code', pairRoute);
+
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error('Server error:', err);
+  res.status(500).json({ error: 'Internal server error' });
+});
+
+app.listen(PORT, () => {
+  console.log(`EXPLORE-MD-PAIR running on http://localhost:${PORT}`);
+  console.log(`GitHub: @LMK360/EXPLORE-MD-PAIR`);
+  console.log(`Author: REDDRAGON`);
+});
+
+module.exports = app;
